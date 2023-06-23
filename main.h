3f3107daa73bae4198c0c9d0e78826e61a733969
Lilo1512 <alaamohamed14101998@outlook.com>
2023-06-23 09:56:58 -0700

    functions1.c

--- main.h
+++ main.h
@@ -9,7 +9,7 @@
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
-<<<<<<< HEAD
+
 void print_buffer(char buffer[], int *buff_ind);
 =======
 int print_char(va_list types, char buffer[],
@@ -22,6 +22,15 @@ int print_int(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 int print_binary(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
->>>>>>> c192e06c93af9e2fc80c3243e91d74ea94cf1e1f
+int print_unsigned(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int print_hexa(va_list types, char map_to[], char buffer[],
+		int flags, char flag_ch, int width, int precision, int size);
 
 #endif
