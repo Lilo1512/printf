5e503b1ffe2f1b1d56c8f8c9b201ee0673e2c802
Lilo1512 <alaamohamed14101998@outlook.com>
2023-06-27 15:48:26 -0700

    update

--- handles_prints.c
+++ handles_prints.c
@@ -3,7 +3,7 @@
 /**
  * handles_prints - Prints an argument based on its type
  * @ami: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
+ * @args: List of arguments to be printed.
  * @ent: ent.
  * @buffer: Buffer array to handle print.
  * @flag: Calculates active flags
@@ -18,7 +18,7 @@ int handles_prints(const char *ami, int *ent, va_list args, char buffer[],
 {
 int a, unknow_len = 0, printed_chars = -1;
 if (ami[*ent] == ami_types[a].ami)
-return (ami_types[a].fn(list, buffer, flag, widths, precisions, sizes));
+return (ami_types[a].fn(args, buffer, flag, widths, precisions, sizes));
 if (ami_types[a].ami == '\0')
 {
 if (ami[*ent] == '\0')
